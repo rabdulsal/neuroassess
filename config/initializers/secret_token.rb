9ce997bf9b5169577be11d6b7bd249f0b4c6034e
Rashad Abdul-Salaam <rashad@getskinclick.com>
2013-01-24 20:02:00 -0600

    Bootstrap integration, and sample Goog Doc

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Neuroassess::Application.config.secret_token = 'de12e2b73d9c26467d6f506ab73ee01d7207c0cc10a36e7d25eec487a24b9d525bce8494ddce79f3b7b220efd54054858e8411ed1277d173610fedc6407f58b6'
